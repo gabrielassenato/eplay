144113b6079cfc683ed34d7fcf504bcaaf1715b1
Gabriel Assenato <gabrielassenato@gmail.com>
2024-07-02 17:01:38 -0300

    Ajuste no código

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,81 +1,81 @@
-import { Container, FooterSection, Link, Links, SectionTitle } from './styles'
+import * as S from './styles'
 
 const currentYear = new Date().getFullYear()
 
 const Footer = () => (
-  <Container>
+  <S.Container>
     <div className="container">
-      <FooterSection>
-        <SectionTitle>Categorias</SectionTitle>
-        <Links>
+      <S.FooterSection>
+        <S.SectionTitle>Categorias</S.SectionTitle>
+        <S.Links>
           <li>
-            <Link
+            <S.Link
               title="Clique aqui para acessar jogos de RPG"
               to="/categories#rpg"
             >
               RPG
-            </Link>
+            </S.Link>
           </li>
           <li>
-            <Link
+            <S.Link
               title="Clique aqui para acessar jogos de ação"
               to="/categories#action"
             >
               Ação
-            </Link>
+            </S.Link>
           </li>
           <li>
-            <Link
+            <S.Link
               title="Clique aqui para acessar jogos de esportes"
               to="/categories#sports"
             >
               Esportes
-            </Link>
+            </S.Link>
           </li>
           <li>
-            <Link
+            <S.Link
               title="Clique aqui para acessar jogos de simulação"
               to="/categories#simulation"
             >
               Simulação
-            </Link>
+            </S.Link>
           </li>
           <li>
-            <Link
+            <S.Link
               title="Clique aqui para acessar jogos de luta"
               to="/categories#fight"
             >
               Luta
-            </Link>
+            </S.Link>
           </li>
-        </Links>
-      </FooterSection>
-      <FooterSection>
-        <SectionTitle>
+        </S.Links>
+      </S.FooterSection>
+      <S.FooterSection>
+        <S.SectionTitle>
           Acesso Rápido
-          <Links>
+          <S.Links>
             <li>
-              <Link
+              <S.Link
                 title="Clique aqui para acessar a seção de promoções"
                 to="/#on-sale"
               >
                 Promoções
-              </Link>
+              </S.Link>
             </li>
             <li>
-              <Link
+              <S.Link
                 title="Clique aqui para acessar a seção em breve"
                 to="/#coming-soon"
               >
                 Em breve
-              </Link>
+              </S.Link>
             </li>
-          </Links>
-        </SectionTitle>
-      </FooterSection>
+          </S.Links>
+        </S.SectionTitle>
+      </S.FooterSection>
       <p>{currentYear} - &copy; E-PLAY Todos os direitos reservados</p>
     </div>
-  </Container>
+  </S.Container>
 )
 
 export default Footer
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,12 +1,13 @@
 import Button from '../Button'
 import Tag from '../Tag'
-import { formataPreco } from '../ProductsList'
+import { parseToBrl } from '../../utils'
 
-import { Banner, Infos } from './styles'
 import { Game } from '../../pages/Home'
 import { useDispatch } from 'react-redux'
 
 import { add, open } from '../../store/reducers/cart'
+
+import * as S from './styles'
 
 type Props = {
   game: Game
@@ -21,22 +22,20 @@ const Hero = ({ game }: Props) => {
   }
 
   return (
-    <Banner style={{ backgroundImage: `url(${game.media.cover})` }}>
+    <S.Banner style={{ backgroundImage: `url(${game.media.cover})` }}>
       <div className="container">
         <div>
           <Tag>{game.details.category}</Tag>
           <Tag>{game.details.system}</Tag>
         </div>
 
-        <Infos>
+        <S.Infos>
           <h2>{game.name}</h2>
           <p>
             {game.prices.discount && (
-              <span>De {formataPreco(game.prices.old)}</span>
+              <span>De {parseToBrl(game.prices.old)}</span>
             )}
-            {game.prices.current && (
-              <>Por {formataPreco(game.prices.current)}</>
-            )}
+            {game.prices.current && <>Por {parseToBrl(game.prices.current)}</>}
           </p>
           {game.prices.current && (
             <Button
@@ -48,9 +47,9 @@ const Hero = ({ game }: Props) => {
               Adicionar ao carrinho
             </Button>
           )}
-        </Infos>
+        </S.Infos>
       </div>
-    </Banner>
+    </S.Banner>
   )
 }
 
